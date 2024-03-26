f3d405a5aea9c4e4b119a446dd4d415b3725dd91
Luyandra_Branco <94245234+LuyandraBranco@users.noreply.github.com>
2024-03-26 17:51:38 +0100

    Radio atualizada

--- App.tsx
+++ App.tsx
@@ -1,106 +1,54 @@
 import { StatusBar } from "expo-status-bar";
-import { useRef, useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import { Howl, Howler } from "howler";
-
-const stations = [
-  {
-    freq: "99.1",
-    title: "Radio Mais",
-    src: "https://radios.justweb.pt/8050/stream/?1685627470876",
-    howl: null,
-  },
-  {
-    freq: "81.4",
-    title: "Radio Escola",
-    src: "https://radios.vpn.sapo.pt/AO/radio1.mp3",
-    howl: null,
-  },
-  {
-    freq: "89.9",
-    title: "Radio Lac",
-    src: "https://radios.vpn.sapo.pt/AO/radio14.mp3?1685629053605 ",
-    howl: null,
-  },
-];
-
-const playStation = (index: any) => {
-  stopCurrentStation();
-
-  const station = stations[index];
-  let sound = station.howl;
-
-  if (!sound) {
-    sound = new Howl({
-      src: [station.src],
-      html5: true,
-      format: ["mp3", "aac"],
-    });
-    stations[index].howl = sound;
-    howlInstances.current[index] = sound;
-  }
-
-  sound.play();
-  setCurrentStationIndex(index);
-};
-
-const stopCurrentStation = () => {
-  if (currentStationIndex !== null) {
-    const sound = howlInstances.current[currentStationIndex];
-    if (sound) {
-      sound.stop();
-    }
-    setCurrentStationIndex(null);
-  }
-};
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import RadioPlayer from "./RadioPlayer";
 
 export default function App() {
-  const [currentStationIndex, setCurrentStationIndex] = useState(null);
-  const howlInstances = useRef([]);
+  const data = [
+    {
+      freq: "99.1",
+      title: "Radio Mais",
+      src: "https://radios.justweb.pt/8050/stream/?1685627470876",
+    },
+    {
+      freq: "81.4",
+      title: "Radio Escola",
+      src: "https://radios.vpn.sapo.pt/AO/radio1.mp3",
+    },
+    {
+      freq: "89.9",
+      title: "Radio Lac",
+      src: "https://radios.vpn.sapo.pt/AO/radio14.mp3?1685629053605",
+    },
+  ];
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 24, fontWeight: "bold", marginBottom: 20 }}>
-        Rádio ManJack
-      </Text>
-      {stations.map((station, index) => (
-        <TouchableOpacity
-          key={index}
-          onPress={() => playStation(index)}
-          style={{
-            padding: 10,
-            marginVertical: 5,
-            backgroundColor:
-              currentStationIndex === index
-                ? "rgba(255, 255, 255, 0.33)"
-                : "transparent",
-          }}
-        >
-          <Text style={{ fontSize: 18, fontWeight: "bold" }}>
-            {station.freq} - {station.title}
-          </Text>
-          <Text style={{ fontSize: 14, color: "gray" }}>
-            {currentStationIndex === index ? "LIVE" : ""}
-          </Text>
-          {currentStationIndex === index && (
-            <View style={{ flexDirection: "row", marginTop: 5 }}>
-              <View style={styles.rect} />
-              <View style={styles.rect} />
-              <View style={styles.rect} />
-              <View style={styles.rect} />
-              <View style={styles.rect} />
-            </View>
-          )}
-        </TouchableOpacity>
-      ))}
+    <View style={styles.container}>
+      <Text style={styles.title}>Radio ISPTEC</Text>
+
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.freq}
+        renderItem={({ item }) => (
+          <RadioPlayer freq={item.freq} title={item.title} src={item.src} />
+        )}
+      />
     </View>
   );
 }
 
-const styles = {
-  rect: {
-    width: 10,
-    height: 10,
-    backgroundColor: "black",
-    marginHorizontal: 2,
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121214",
+    justifyContent: "center",
+    // alignItems: 'center',
+    paddingHorizontal: 24,
+    paddingTop: 56,
   },
-};
+  title: {
+    color: "#FFF",
+    fontSize: 28,
+    fontWeight: "700",
+    marginBottom: 24,
+  },
+});
