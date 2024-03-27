4209e479515fc8d9717963fe37cdfec2cbe11f98
manjjack <manjjackson@hotmail.com>
2024-03-27 03:08:22 +0100

    RadioStation added

--- App.tsx
+++ App.tsx
@@ -1,32 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import RadioPlayer from "./RadioPlayer";
+import radioStations from "./radioStation";
 
 export default function App() {
-  const data = [
-    {
-      freq: "99.1",
-      title: "Radio Mais",
-      src: "https://radios.justweb.pt/8050/stream/?1685627470876",
-    },
-    {
-      freq: "81.4",
-      title: "Radio Escola",
-      src: "https://radios.vpn.sapo.pt/AO/radio1.mp3",
-    },
-    {
-      freq: "89.9",
-      title: "Radio Lac",
-      src: "https://radios.vpn.sapo.pt/AO/radio14.mp3?1685629053605",
-    },
-  ];
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Radio ISPTEC</Text>
+      <Text style={styles.title}>Radio Manjack</Text>
 
       <FlatList
-        data={data}
+        data={radioStations}
         keyExtractor={(item) => item.freq}
         renderItem={({ item }) => (
           <RadioPlayer freq={item.freq} title={item.title} src={item.src} />
